c69e2d9ff174cbc800ea6431fdd637da5fbb4b49
Castiant02 <antonio.castillo03@utrgv.edu>
2025-02-02 02:18:25 +0000

    done

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
+public class form{
+    public static void(String[] args){
+        Scanner scanner= new Scanner(System.in);
 
+        System.out.print("enter name here: ");
+        String name=scanner.nextLine();
+
+        System.out.print("enter age: ");
+        int age=scanner.nextint();
+
+        System.out.print("Enter weight");
+        float weight=scanner.nextfloat();
+
+        System.out.print("Do you code? (True or false)");
+        boolean cool =scanner.nextboolean();
+
+        System.out.printin("\n--Medical form")
+        System.out.printin("name"+name);
+        System.out.printin("Age" +age);
+        System.out.printin("weight" + weight);
+        System.out.printin("Coder" + cool);
+        scanner.close();
+
+    }
+}
